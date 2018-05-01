fc4c7b4d4b85a460bfcc5753afd6b10dadb673da
pawel112 <pawel112@users.noreply.github.com>
2018-05-01 13:27:14 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,8 +4,8 @@ module.exports = {
 	'mail_password':	'',
 	'mail_port':		993,
 	'mail_port_stmp':	587,
-	'db_host':			'',
-	'db_name':			'',
-	'db_login':			'',
+	'db_host':		'',
+	'db_name':		'',
+	'db_login':		'',
 	'db_password':		''
 };
